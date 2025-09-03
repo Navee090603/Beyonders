38a265215eb09bce1cd3c7feb96de29623f1ff31
KARTHIKA VARSHINI <karthikav@infinite.com>
2025-09-03 11:37:27 +0530

    Refunds,Reports,SupportTicket UI

---
+++ Design_IKart/IKart_Design/Areas/Admin/Controllers/SupportTicketController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IKart_Design.Areas.Admin.Controllers
+{
+    public class SupportTicketController : Controller
+    {
+        // GET: Admin/SupportTicket
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
